ff04cf5851efb2dc4c56e01f896fc68a59fe858a
broge2 <btcr1990@gmail.com>
2017-03-28 18:22:59 -0500

    Changed the rules for the simple battle simulator
    The defender now has a roll check plus speed to beat for the attacker.

--- src/simulator/bs/SimpleBattleSimulator.java
+++ src/simulator/bs/SimpleBattleSimulator.java
@@ -3,17 +3,12 @@ package simulator.bs;
 public class SimpleBattleSimulator extends BattleSimulator {
 	
 	@Override
-	/**
-	 * This set of rules rolls the dice for the attacker and adds their speed.
-	 * This then tests the condition for the result of the roll plus attacker's
-	 * speed and see's if it is greater than the defender's speed. This may need
-	 * some modification but it's a start.
-	 */
 	protected void rules(Character attacker, Character defender) {
-		System.out.printf("Testing if hit.  Must beat %d\n", defender.getSpeed());
-		int dieRoll = Dice.roll(100);
-		System.out.printf("The result is %d + %d = %d\n", dieRoll, attacker.getSpeed(), (dieRoll + attacker.getSpeed()));
-		if(dieRoll + attacker.getSpeed() > defender.getSpeed()){
+		int attackerHitCheck = Dice.roll(100) + attacker.getSpeed();
+		int defenderDefenseCheck = Dice.roll(100) + defender.getSpeed();
+		System.out.printf("Testing if hit.  Must beat %d\n", defenderDefenseCheck);
+		System.out.printf("The result is %d + %d = %d\n", attackerHitCheck, attacker.getSpeed(), attackerHitCheck);
+		if(attackerHitCheck > defenderDefenseCheck){
 			System.out.printf("Testing if crit. Must be <= %d\n", attacker.getCritChance());
 			if(Dice.roll(100) <= attacker.getCritChance()){
 				attacker.phyAttack(defender, true);
