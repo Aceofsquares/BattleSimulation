8eaeecf0c9a969b131ce0c4268478d9706f68bac
Brian-Thomas Rogers <btcr1990@gmail.com>
2017-03-28 19:31:53 -0500

    Correctly display math for attacker

--- src/simulator/bs/SimpleBattleSimulator.java
+++ src/simulator/bs/SimpleBattleSimulator.java
@@ -1,16 +1,17 @@
 package simulator.bs;
 
 public class SimpleBattleSimulator extends BattleSimulator {
-	
+
 	@Override
 	protected void rules(Character attacker, Character defender) {
 		int attackerHitCheck = Dice.roll(100) + attacker.getSpeed();
 		int defenderDefenseCheck = Dice.roll(100) + defender.getSpeed();
 		System.out.printf("Testing if hit.  Must beat %d\n", defenderDefenseCheck);
-		System.out.printf("The result is %d + %d = %d\n", attackerHitCheck, attacker.getSpeed(), attackerHitCheck);
-		if(attackerHitCheck > defenderDefenseCheck){
+		System.out.printf("The result is %d + %d = %d\n", attackerHitCheck - attacker.getSpeed(), attacker.getSpeed(),
+				attackerHitCheck);
+		if (attackerHitCheck > defenderDefenseCheck) {
 			System.out.printf("Testing if crit. Must be <= %d\n", attacker.getCritChance());
-			if(Dice.roll(100) <= attacker.getCritChance()){
+			if (Dice.roll(100) <= attacker.getCritChance()) {
 				attacker.phyAttack(defender, true);
 			}
 			attacker.phyAttack(defender, false);
