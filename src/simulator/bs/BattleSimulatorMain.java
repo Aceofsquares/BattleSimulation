a80dfcc19a819ca49d44f993d4b4163ed308c253
Brian-Thomas Rogers <btcr1990@gmail.com>
2017-03-28 19:42:06 -0500

    Includes who is rolling and what round you are in

--- src/simulator/bs/BattleSimulatorMain.java
+++ src/simulator/bs/BattleSimulatorMain.java
@@ -8,20 +8,24 @@ public class BattleSimulatorMain {
 	
 	public static void main(String[] args) {
 		input = new Scanner(System.in);
+		int round = 1;
 		//Name, Speed, phyDef, phyAtt, critChance, maxLife, isAlive
 		Human brian = new Human("Brian", 8, 17, 17, 4, 18, true);
-		Monster banshee = new Monster("Banshee", 3, 10, 5, 35, 15, true);
+		Monster banshee = new Monster("Banshee", 3, 10, 5, 55, 15, true);
 		BattleSimulator battleSim = new SimpleBattleSimulator(); //Change to your Subclassed simulator.
 		while(brian.isAlive() && banshee.isAlive()){
 			waitForEnter();
+			System.out.printf("\nROUND %d\n", round);
 			System.out.printf("\n--%s's turn!--\n", brian.getName());
 			battleSim.battle(brian, banshee);
 			printBattleInformation(banshee);
+			waitForEnter();
 			if(banshee.isAlive()){
 				System.out.printf("\n--%s's turn!--\n", banshee.getName());
 				battleSim.battle(banshee, brian);
 				printBattleInformation(brian);
 			}
+			round += 1;
 		}
 		
 		if(!brian.isAlive()){
--- src/simulator/bs/SimpleBattleSimulator.java
+++ src/simulator/bs/SimpleBattleSimulator.java
@@ -4,7 +4,9 @@ public class SimpleBattleSimulator extends BattleSimulator {
 
 	@Override
 	protected void rules(Character attacker, Character defender) {
+		System.out.printf("%s is rolling: ", attacker.getName());
 		int attackerHitCheck = Dice.roll(100) + attacker.getSpeed();
+		System.out.printf("%s is rolling: ", defender.getName());
 		int defenderDefenseCheck = Dice.roll(100) + defender.getSpeed();
 		System.out.printf("Testing if hit.  Must beat %d\n", defenderDefenseCheck);
 		System.out.printf("The result is %d + %d = %d\n", attackerHitCheck - attacker.getSpeed(), attacker.getSpeed(),
